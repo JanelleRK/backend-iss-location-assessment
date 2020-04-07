c5c5cde47ff7703f93e07294a2f1ea922ec3e227
Janelle Kuhns <j3ckuhns@gmail.com>
2020-04-06 17:58:08 -0700

    updating main

--- iss.py
+++ iss.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+import requests
+import sys
+
 
 __author__ = 'Janelle Kuhns with help from demo'
 
 
 def main():
-    pass
+    astronaut_info = requests.get("http://api.open-notify.org/astros.json")
+    astronaut_info.text
+    print(astronaut_info)
 
 
 if __name__ == '__main__':
